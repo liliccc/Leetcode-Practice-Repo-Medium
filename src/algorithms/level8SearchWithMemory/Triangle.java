9caa4ace9967be967e1a5cb20d3b3f87ba67d9bd
liliccc <xianj.chen@qq.com>
2021-12-11 17:19:30 +0800

    12.11

--- src/algorithms/level8SearchWithMemory/Triangle.java
+++ src/algorithms/level8SearchWithMemory/Triangle.java
@@ -12,18 +12,18 @@ public class Triangle {
         return divideConquer(triangle, 0 , 0, new HashMap<>());
     }
 
-    private int divideConquer(int[][] triangle, int x, int y, Map<Integer, Integer> map) {
+    private int divideConquer(int[][] triangle, int x, int y, Map<Integer, Integer> memo) {
         if (x == triangle.length) {
             return 0;
         }
         int hashCode = x * triangle.length + y;
-        if (map.containsKey(hashCode)) {
-            return map.get(hashCode);
+        if (memo.containsKey(hashCode)) {
+            return memo.get(hashCode);
         }
-        int left = divideConquer(triangle, x + 1, y, map);
-        int right = divideConquer(triangle, x+ 1, y + 1, map);
+        int left = divideConquer(triangle, x + 1, y, memo);
+        int right = divideConquer(triangle, x+ 1, y + 1, memo);
         // add triangle[x][y]
-        map.put(hashCode, Math.min(left, right) + triangle[x][y]);
-        return map.get(hashCode);
+        memo.put(hashCode, Math.min(left, right) + triangle[x][y]);
+        return memo.get(hashCode);
     }
 }
