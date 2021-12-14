6d57da840caabc10e31edc5dc7f6e98c5c0829d7
liliccc <xianj.chen@qq.com>
2021-12-14 10:43:49 +0800

    12.13

---
+++ src/algorithms/level11BinarySearchReview/CopyBooks.java
@@ -0,0 +1,12 @@
+package algorithms.level11BinarySearchReview;
+
+public class CopyBooks {
+    /**
+     * @param pages: an array of integers
+     * @param k: An integer
+     * @return: an integer
+     */
+    public int copyBooks(int[] pages, int k) {
+        return 0;
+    }
+}
