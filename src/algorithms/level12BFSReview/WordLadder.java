76b912dda871e8c1c8e8b379ee3be43b44e5d94b
liliccc <xianj.chen@qq.com>
2021-12-28 17:39:00 +0800

    12.28

---
+++ src/algorithms/level12BFSReview/WordLadder.java
@@ -0,0 +1,15 @@
+package algorithms.level12BFSReview;
+
+import java.util.Set;
+
+public class WordLadder {
+    /*
+     * @param start: a string
+     * @param end: a string
+     * @param dict: a set of string
+     * @return: An integer
+     */
+    public int ladderLength(String start, String end, Set<String> dict) {
+        return 0;
+    }
+}
