88beb5e4694c24df5e2373b5f086179f806c48d9
liliccc <xianj.chen@qq.com>
2021-12-29 17:27:07 +0800

    12.29

---
+++ src/algorithms/level12BFSReview/LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,30 @@
+package algorithms.level12BFSReview;
+
+public class LowestCommonAncestorofaBinarySearchTree {
+    /**
+     * @param root: root of the tree
+     * @param p: the node p
+     * @param q: the node q
+     * @return: find the LCA of p and q
+     */
+     // BST => root.left <= root && root.right >= root, non-recursive
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+        int upperBound = Math.max(p.val, q.val);
+        int lowerBound = Math.min(p.val, q.val);
+        while (root != null) {
+            if (root.val <= upperBound && lowerBound <= root.val) {
+                return root;
+            }
+            else if (root.val > upperBound) {
+                root = root.left;
+            }
+            else {
+                root = root.right;
+            }
+        }
+        return null;
+    }
+}
