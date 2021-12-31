bab65dc0ba140ca61166312b02198146d3b7efd2
liliccc <xianj.chen@qq.com>
2021-12-31 13:31:08 +0800

    12.31

--- src/algorithms/level11BinarySearchReview/MedianoftwoSortedArrays.java
+++ src/algorithms/level11BinarySearchReview/MedianoftwoSortedArrays.java
@@ -7,8 +7,60 @@ public class MedianoftwoSortedArrays {
      * @return: a double whose format is *.5 or *.0
      */
     public double findMedianSortedArrays(int[] A, int[] B) {
-        // write your code here
+        if (A == null || B == null) {
+            return -1;
+        }
+        int n = A.length + B.length;
+        if (n % 2 == 0) {
+            return (findKthNumber(A, B, n / 2) + findKthNumber(A, B, n / 2 + 1)) / 2.0;
+        }
+        return findKthNumber(A, B, n / 2 + 1);
+    }
 
-        return 0;
+    // get the middle value of 2 arrays
+    private int findKthNumber(int[] A, int[] B, int k) {
+        if (A.length == 0) {
+            return B[k - 1];
+        }
+        if (B.length == 0) {
+            return A[k - 1];
+        }
+        int min = Math.min(A[0], B[0]);
+        int max = Math.max(A[A.length - 1], B[B.length - 1]);
+        while (min + 1 < max) {
+            int mid = min + (max - min) / 2;
+            // <=
+            if (findIndexEqualOrSmallerThanMid(A, mid) + findIndexEqualOrSmallerThanMid(B, mid) < k) {
+                min = mid;
+            }
+            else {
+                max = mid;
+            }
+        }
+        if (findIndexEqualOrSmallerThanMid(A, min) + findIndexEqualOrSmallerThanMid(B, min) >= k) {
+            return min;
+        }
+        return max;
+    }
+
+    // find the index of an array which is the closest to the given number
+    private int findIndexEqualOrSmallerThanMid(int[] array, int number) {
+        int start = 0, end = array.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (array[mid] <= number) {
+                start = mid;
+            }
+            else {
+                end = mid;
+            }
+        }
+        if (array[start] > number) {
+            return start;
+        }
+        if (array[end] > number) {
+            return end;
+        }
+        return array.length;
     }
 }
---
+++ src/algorithms/level12BFSReview/BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,14 @@
+package algorithms.level12BFSReview;
+
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversalII {
+    /**
+     * @param root: A tree
+     * @return: buttom-up level order a list of lists of integer
+     */
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        // write your code here
+        return null;
+    }
+}
