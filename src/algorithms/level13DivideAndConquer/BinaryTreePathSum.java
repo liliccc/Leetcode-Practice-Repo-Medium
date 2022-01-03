4f701dd6dcf40a54ed022b5b1f49c79e888854f5
liliccc <xianj.chen@qq.com>
2022-01-03 11:25:25 +0800

    1.2

---
+++ src/algorithms/level13DivideAndConquer/BinaryTreePathSum.java
@@ -0,0 +1,38 @@
+package algorithms.level13DivideAndConquer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePathSum {
+    /*
+     * @param root: the root of binary tree
+     * @param target: An integer
+     * @return: all valid paths
+     */
+     // divide and conquer: big problem -> smaller problems, target is changing
+    public List<List<Integer>> binaryTreePathSum(TreeNode root, int target) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (root == null) {
+            return results;
+        }
+        // handle leaf, if root.val != target , return an empty ArrayList
+        if (root.left == null && root.right == null && root.val == target) {
+            List<Integer> nodeList = new ArrayList<>();
+            nodeList.add(root.val);
+            results.add(nodeList);
+            return results;
+        }
+        List<List<Integer>> leftResults = binaryTreePathSum(root.left, target - root.val);
+        List<List<Integer>> rightResults = binaryTreePathSum(root.right, target - root.val);
+        for (List<Integer> leftList : leftResults) {
+            leftList.add(0, root.val);
+            results.add(leftList);
+        }
+        for (List<Integer> rightList : rightResults) {
+            rightList.add(0, root.val);
+            results.add(rightList);
+        }
+        return results;
+    }
+}
+
---
+++ src/algorithms/level13DivideAndConquer/TreeNode.java
@@ -0,0 +1,11 @@
+package algorithms.level13DivideAndConquer;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
