25d700ac2895e751bf30026bb60ba15365af86c0
liliccc <xianj.chen@qq.com>
2022-01-25 00:11:23 +0800

    1.24

---
+++ src/algorithms/level16DynamicProgrammingReview/LongestCommonSubsequence.java
@@ -0,0 +1,27 @@
+package algorithms.level16DynamicProgrammingReview;
+
+public class LongestCommonSubsequence {
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: The length of longest common subsequence of A and B
+     */
+    public int longestCommonSubsequence(String A, String B) {
+        if (A == null || B == null) {
+            return 0;
+        }
+        int Alen = A.length(), Blen = A.length();
+        // state + initialization
+        int[][] dp = new int[Alen + 1][Blen + 1];
+        // function
+        for (int i = 1; i < Alen + 1; i ++) {
+            for (int j = 1; j < Blen + 1; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                if (A.charAt(i -1) == B.charAt(j -1)) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return dp[Alen][Blen];
+    }
+}
