f2f89c741030530f597a8d9670b2bcc6db93be75
liliccc <xianj.chen@qq.com>
2022-01-26 00:12:54 +0800

    1.25

---
+++ src/algorithms/level16DynamicProgrammingReview/WordBreak.java
@@ -0,0 +1,39 @@
+package algorithms.level16DynamicProgrammingReview;
+
+import java.util.Set;
+
+public class WordBreak {
+    /**
+     * @param s: A string
+     * @param wordSet: A dictionary of words dict
+     * @return: A boolean
+     */
+    public boolean wordBreak(String s, Set<String> wordSet) {
+        if (s == null) {
+            return true;
+        }
+        int maxLength = 0;
+        for (String word : wordSet) {
+            maxLength = Math.max(maxLength, word.length());
+        }
+        int size = s.length();
+        boolean[] dp = new boolean[size + 1];
+        dp[0] = true;
+        for (int i = 1; i < size + 1; i++) {
+            for (int len = 1; len <= maxLength; len++) {
+                if (i < len) {
+                    break;
+                }
+                if (!dp[i - len]) {
+                    continue;
+                }
+                String word = s.substring(i - len, i);
+                if (wordSet.contains(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[size];
+    }
+}
